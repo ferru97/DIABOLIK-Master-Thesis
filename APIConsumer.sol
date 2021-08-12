749812187705132deb198e38498f62d66acf3659
ferru97 <vitof970@gmail.com>
2021-08-12 20:09:08 +0200

    implemented simple http job

---
+++ APIConsumer.sol
@@ -0,0 +1,70 @@
+pragma solidity 0.6.6;
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+
+contract APIConsumer is ChainlinkClient {
+  
+    bytes32 public  random_number;
+    address public oracle;
+    bytes32 public jobId;
+    uint256 public fee;
+    
+    constructor(address _oracle, string memory _jobID) public {
+        setPublicChainlinkToken();
+    //Fill in the Oracle address we just deployed
+        oracle = _oracle;
+    //Fill in the address we just created
+        jobId = stringToBytes32(_jobID);
+    //The minimum fee paid to Oracle can be found in the configuration page minimum_ CONTRACT_ View the payment field
+        fee = 1 * 10 ** 18; // 1 LINK
+    }
+    
+    function randNum() public returns (bytes32 requestId) 
+    {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        
+        //add query params
+        request.add("queryParams","num=1&col=1&base=10&format=plain&rnd=new&min=10&max=100");
+        
+        //Send request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+    
+    function randNumNoParams() public returns (bytes32 requestId) 
+    {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        
+        //add query params
+        //request.add("queryParams","num=1&col=1&base=10&format=plain&rnd=new&min=10&max=100");
+        
+        //Send request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+    
+    function fulfill(bytes32 _requestId, bytes32 _num) public recordChainlinkFulfillment(_requestId)
+    {
+        random_number = _num;
+    }
+    
+    function setOracle(address _oracle) public{
+        oracle = _oracle;
+    }
+    
+    function setJobID(string memory _jobID) public{
+        jobId = stringToBytes32(_jobID);
+    }
+    
+    function setFee(uint256 _fee) public{
+        fee = _fee;
+    }
+    
+    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+}
